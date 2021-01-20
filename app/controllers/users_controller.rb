9cbdf4e18588caec6897b81798a35fa18b74e7eb
Joe@Edood <Doode1524@gmail.com>
2021-01-19 21:05:21 -0500

    user controller actions init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    def find_user
+        
+    end
+
+    def user_params
+        
+    end
 end
