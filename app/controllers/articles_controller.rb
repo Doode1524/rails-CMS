c2c245e2c1131a313b61291ce6bd32c0b7de27ff
Joe@Edood <Doode1524@gmail.com>
2021-01-20 03:25:42 -0500

    articles index init

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,11 @@
 class ArticlesController < ApplicationController
+    
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find_by_id(params[:id])
+    end
+    
 end
