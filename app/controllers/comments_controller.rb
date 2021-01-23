f8187c4010c8ad5835af6f8bc53352bbbf19b436
Joe@Edood <Doode1524@gmail.com>
2021-01-22 23:50:26 -0500

    comments set up

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,59 @@
 class CommentsController < ApplicationController
-
+    
+    before_action :find_comment, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        if params[:article_id]
+            @comments = Article.find(params[:article_id]).comments     
+            @article = Article.find(params[:article_id])   
+        else
+            @comments = Comment.all
+        end
+    end
+    
     def new
         @comment = Comment.new
     end
 
-    def index
-        @comments = Comment.all
-    end
-
-    def show
-        @comment = Comment.find_by_id(params[:id])
-    end
-
     def create
-        @comment = Comment.new(comment_params)
-        if @comment.save
-            redirect_to @comment
+        comment = Comment.new(comment_params)
+        article = comment.article
+        comment.user_id = current_user.id
+     
+        if comment.save
+        redirect_to article_comments_path(article)
         else
-            render :new
+            render '/articles/show'
         end
     end
 
-    def comment_params
-        params.require(:comment).permit(:content, :article_id, :user_id)
+
+    def edit
+
     end
+
+    def update
+        if @comment.update
+            redirect_to comment_path(@comment)
+        else
+            render :new           
+            
+        end
+    end
+
+    def delete
+        @comment.delete
+        redirect_to :new
+    end
+
+    private
+    def find_comment
+        @comment = Comment.find_by_id(params[:id])
+    end
+
+    def comment_params
+        params.require(:comment).permit(:content, :article_id)
+    end
+
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,17 @@
 class Article < ApplicationRecord
     has_many :comments
     has_many :users, through: :comments
-
-    validates_uniqueness_of :title, :description
+    validates :title, uniqueness: true
+    accepts_nested_attributes_for :comments
     paginates_per 20
+
+def self.by_source(source)
+    where(source: source)
+end 
+
+
+def self.all_sources
+    Article.pluck(:source).uniq.sort
+end
 
 end
