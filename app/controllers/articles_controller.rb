f5bd08c5753ce092b27fc0e2d8774e7e3f9c9eb4
Joe@Edood <Doode1524@gmail.com>
2021-01-22 19:57:19 -0500

    comments init2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,7 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find_by_id(params[:id])
+      
     end
 
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+
+    def new
+        @comment = Comment.new
+    end
+
+    def index
+        @comments = Comment.all
+    end
+
+    def show
+        @comment = Comment.find_by_id(params[:id])
+    end
+
+    def create
+        @comment = Comment.new(comment_params)
+        if @comment.save
+            redirect_to @comment
+        else
+            render :new
+        end
+    end
+
+    def comment_params
+        params.require(:comment).permit(:content, :article_id, :user_id)
+    end
 end
