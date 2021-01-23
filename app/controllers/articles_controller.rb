27ee8e041cbf015c9d44b9f5722f904253d744f7
Joe@Edood <Doode1524@gmail.com>
2021-01-22 23:49:43 -0500

    articles index edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,42 @@ class ArticlesController < ApplicationController
     
     def index
         Api.load_data
-        articles = Article.all.sort_by(&:published_at).reverse
-        @articles = Kaminari.paginate_array(articles).page(params[:page]).per(25)
+
+        if !params[:source].blank?
+            articles = Article.by_source(params[:source])
+            @articles = Kaminari.paginate_array(articles).page(params[:page]).per(25)
+        else
+            articles = Article.all.sort_by(&:published_at).reverse
+            @articles = Kaminari.paginate_array(articles).page(params[:page]).per(25)
+        end
     end
 
     def show
-        @article = Article.find_by_id(params[:id])
-      
+        find_article
+        @comment = Comment.new
     end
 
+    def create
+        redirect_to articles_path
+    end
+
+    def update
+        find_article
+        
+        if @article.update(article_params)
+            redirect_to articles_path
+        else
+            render :show
+        end
+
+    end
+
+    private
+    def find_article
+        @article = Article.find_by_id(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(comment_attributes: [:content])
+    end
 end
