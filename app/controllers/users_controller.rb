d585b0c518895520519e33336451b87f2dd857a4
Joe@Edood <Doode1524@gmail.com>
2021-01-22 23:51:02 -0500

    nested routes for articles_comments path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,27 @@
 class UsersController < ApplicationController
 
-    def index   
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    def index
         @users = User.all
     end
-
-    def show
-        find_user
-    end
-
+    
     def new
         @user = User.new
     end
 
     def create
-        @user = User.new(user_params)
-
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            redirect_to user_path(user)
         else
             render :new
         end
+    end
+
+    def show
+        
     end
 
     def edit
@@ -28,18 +29,27 @@ class UsersController < ApplicationController
     end
 
     def update
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            render :new           
+        end
 
     end
 
-    def destroy
-
+    def delete
+        @user.delete
+        redirect_to :new
     end
 
-    def find_user   
+    private
+
+    def find_user
         @user = User.find_by_id(params[:id])
     end
 
     def user_params
-        params.require(:user).permit(:username, :email, :password_digest)
+        params.require(:user).permit(:username, :email, :password)
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :articles, only: [:show] do
+    resources :comments, only: [:show, :index]
+  end
   resources :comments
-  resources :articles
   resources :users
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
