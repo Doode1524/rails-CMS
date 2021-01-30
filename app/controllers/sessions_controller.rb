e88960a8b2d6fb5e0ce5b1b0b5c62dc604689b2f
Joe@Edood <Doode1524@gmail.com>
2021-01-29 23:25:37 -0500

    bug fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user_path(user)
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to user_path(@user)
     else
       render 'new'
     end
